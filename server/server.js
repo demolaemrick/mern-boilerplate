a3b14d22aee5483bf9fff451c59ed6358e0e8c94
Emrick19 <emricky17@gmail.com>
2021-01-01 02:37:16 +0100

    removed ejs import

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 require('dotenv').config() //for env file to load up
 
 const express = require('express')
-const ejs = require('ejs')
 const mongoose = require('mongoose')
 
 const app = express();
 app.use(bodyparser.json()); //utilizes the body-parser package
-
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Connected to database...'))
